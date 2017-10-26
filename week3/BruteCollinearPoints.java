b406fd7b125824ab863fb2c01f95e63873e14090
Jose Jimenez <jose.jimenez@elemetrica.com>
2017-10-26 16:27:00 -0400

    start of week3

---
+++ week3/BruteCollinearPoints.java
@@ -0,0 +1,18 @@
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {
+        for(int i = 0; i < points.length; i++) {
+            for(int j = i+1; j < points.length; j++) {
+                for(int k = j+1; k < points.length; k++) {
+                    for(int l = k+1; l < points.length; l++) {
+                        Point p = points[i];
+                        Point q = points[j];
+                        Point r = points[k];
+                        Point s = points[l];
+
+
+                    }
+                }
+            }
+        }
+    }
+ }
